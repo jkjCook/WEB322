5b2d5791d5f5fb04505d81511b5ef38c42075876
Justin Cook <jkjcook@gmail.com>
2017-05-28 19:38:44 -0400

    working test

--- server.js
+++ server.js
@@ -9,7 +9,6 @@
 * Online (Heroku) Link: https://vast-stream-84912.herokuapp.com/
 *
 ********************************************************************************/
-console.log("testing!!");
 
 var express = require("express");
 var app = express();

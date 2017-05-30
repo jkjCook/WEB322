edafca05ccbb54057dc40cb427a441f4cd025f4b
Justin Cook <jkjcook@gmail.com>
2017-05-30 03:31:04 -0400

    Formatted and added routes

--- data-service.js
+++ data-service.js
@@ -3,123 +3,109 @@ var fs = require("fs");
 var employees = [];
 var departments = [];
 
-module.exports.initialize = function(){
+module.exports.initialize = function () {
     var loadError = false;
 
     fs.readFile('./data/employees.json', 'utf8', (err, data) => {
         if (err) loadError = true;
         else employees = JSON.parse(data);
     });
-    
+
     fs.readFile('./data/departments.json', 'utf8', (err, data) => {
         if (err) loadError = true;
         else departments = JSON.parse(data);
-    }); 
+    });
 
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            if (loadError) reject();
-            else resolve("Has been resolved");
-        }, 1000);
+    return new Promise(function (resolve, reject) {
+        if (loadError) reject();
+        else resolve("Has been resolved");
     })
 
 }
 
-module.exports.getAllEmployees = function(){
-    return new Promise(function(resolve, reject){
-        if(employees.length == 0) reject("No results returned.");
+module.exports.getAllEmployees = function () {
+    return new Promise(function (resolve, reject) {
+        if (employees.length == 0) reject("No results returned.");
         else {
-            setTimeout(function(){
-                resolve(employees);
-            }, 1000)
+            resolve(employees);
         }
     })
 }
 
-module.exports.getEmployeesByStatus = function(status){
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            if(employees.length == 0) reject("No results returned.");
+module.exports.getEmployeesByStatus = function (status) {
+    return new Promise(function (resolve, reject) {
+        if (employees.length == 0) reject("No results returned.");
 
-            else {
-                var holder = [];
-                for (var i = 0; i < employees.length; i++){
-                        if(employees[i].status == status)
-                            holder.push(employees[i]);
-                }
-                (holder.length != 0) ? resolve(holder) : reject("No results returned.");
+        else {
+            var holder = [];
+            for (var i = 0; i < employees.length; i++) {
+                if (employees[i].status == status)
+                    holder.push(employees[i]);
             }
-        }, 1000) 
+            (holder.length != 0) ? resolve(holder) : reject("No results returned.");
+        }
     })
 }
-module.exports.getEmployeesByDepartment = function(department){
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            if(employees.length == 0) reject("No results returned.");
+module.exports.getEmployeesByDepartment = function (department) {
+    return new Promise(function (resolve, reject) {
+        if (employees.length == 0) reject("No results returned.");
 
-            else {
-                var holder = [];
-                for (var i = 0; i < employees.length; i++){
-                        if(employees[i].department == department)
-                            holder.push(employees[i]);
-                }
-                (holder.length != 0) ? resolve(holder) : reject("No results returned.");
+        else {
+            var holder = [];
+            for (var i = 0; i < employees.length; i++) {
+                if (employees[i].department == department)
+                    holder.push(employees[i]);
             }
-        }, 1000) 
+            (holder.length != 0) ? resolve(holder) : reject("No results returned.");
+        }
     })
 }
-module.exports.getEmployeesByManager = function(manager){
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            if(employees.length == 0) reject("No results returned.");
+module.exports.getEmployeesByManager = function (manager) {
+    return new Promise(function (resolve, reject) {
+        if (employees.length == 0) reject("No results returned.");
 
-            else {
-                var holder = [];
-                for (var i = 0; i < employees.length; i++){
-                        if(employees[i].employeeManagerNum == manager)
-                            holder.push(employees[i]);
-                }
-                (holder.length != 0) ? resolve(holder) : reject("No results returned.");
+        else {
+            var holder = [];
+            for (var i = 0; i < employees.length; i++) {
+                if (employees[i].employeeManagerNum == manager)
+                    holder.push(employees[i]);
             }
-        }, 1000) 
+            (holder.length != 0) ? resolve(holder) : reject("No results returned.");
+        }
     })
 }
-module.exports.getEmployeeByNum = function(num){
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            if(employees.length == 0) reject("No results returned.");
+module.exports.getEmployeeByNum = function (num) {
+    return new Promise(function (resolve, reject) {
+        if (employees.length == 0) reject("No results returned.");
 
-            else {
-                for (var i = 0; i < employees.length; i++){
-                        if(employees[i].employeeNum == num)
-                            resolve(employees[i]);
+        else {
+            for (var i = 0; i < employees.length; i++) {
+                if (employees[i].employeeNum == num) {
+                    resolve(employees[i]);
                 }
             }
-        }, 1000) 
+            reject("No employee found.");
+        }
     })
 }
-module.exports.getManagers = function(){
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            if(employees.length == 0) reject("No results returned.");
+module.exports.getManagers = function () {
+    return new Promise(function (resolve, reject) {
+        if (employees.length == 0) reject("No results returned.");
 
-            else {
-                var holder = [];
-                for (var i = 0; i < employees.length; i++){
-                        if(employees[i].isManager == true)
-                            holder.push(employees[i]);
-                }
-                resolve(holder);
+        else {
+            var holder = [];
+            for (var i = 0; i < employees.length; i++) {
+                if (employees[i].isManager == true)
+                    holder.push(employees[i]);
             }
-        }, 1000) 
+            resolve(holder);
+        }
     })
 }
-module.exports.getDepartments = function(){
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            if(departments.length == 0) reject("No results found.");
-            else resolve(departments);
-        },1000)
+module.exports.getDepartments = function () {
+    return new Promise(function (resolve, reject) {
+        if (departments.length == 0) reject("No results found.");
+        else resolve(departments);
     })
 }
 
--- server.js
+++ server.js
@@ -13,7 +13,7 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-var service = require("./data-service.js");  
+var service = require("./data-service.js");
 var querystring = require("querystring");
 app.use(express.static('public'));
 
@@ -25,65 +25,80 @@ function onHttpStart() {
 }
 
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res){
-  res.sendFile(path.join(__dirname + "/views/home.html"));
-});
-
-//another route for finding home with /home
-app.get("/home", function(req,res){
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 // setup another route to listen on /about
-app.get("/about", function(req,res){
+app.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/about.html"));
 });
-
-app.get("/employees*/", function(req,res){
+// setup a route to listen for employee queries
+app.get("/employees*/", function (req, res) {
   var result = querystring.parse(req.originalUrl, "?", "=");
-  if(result.department){
-    service.getEmployeesByDepartment(result.department).then((data) =>{
+
+  if (result.department) {
+    service.getEmployeesByDepartment(result.department).then((data) => {
       res.json(data);
     }).catch((err) => {
-      res.json({message: err});
+      res.json({ message: err });
     })
   }
-  else if(result.status){
-    service.getEmployeesByStatus(result.status).then(function(data){
+
+  else if (result.status) {
+    service.getEmployeesByStatus(result.status).then(function (data) {
       res.json(data);
+    }).catch((err) => {
+      res.json({ message: err });
     })
   }
-  else if(result.manager){
-    service.getEmployeesByManager(result.manager).then(function(data){
+  else if (result.manager) {
+    service.getEmployeesByManager(result.manager).then(function (data) {
       res.json(data);
+    }).catch((err) => {
+      res.json({ message: err });
     })
   }
-  else{
+  else {
     res.send(result);
   }
 
 });
-app.get("/employee/[0-9]*", function(req,res){
+// setup route to employee plus a numeric value for which employee you are looking for
+app.get("/employee/*", function (req, res) {
   //Had to use a regular substring because the url wasn't in JSON and I couldn't figure out another way
-  var result = req.originalUrl.substring(req.originalUrl.lastIndexOf('/')+1);
-  service.getEmployeeByNum(result).then(function(data){
-      res.json(data);
-    })
+  var result = req.originalUrl.substring(req.originalUrl.lastIndexOf('/') + 1);
+  service.getEmployeeByNum(result).then(function (data) {
+    res.json(data);
+  }).catch((err) => {
+    res.json({ message: err });
+  })
 });
-app.get("/managers", function(req,res){
-  service.getManagers().then(function(data){
-      res.json(data);
-    })
+// setup route for all managers
+app.get("/managers", function (req, res) {
+  service.getManagers().then(function (data) {
+    res.json(data);
+  }).catch((err) => {
+    res.json({ message: err });
+  })
 });
-app.get("/departments", function(req,res){
-  service.getDepartments().then(function(data){
-      res.json(data);
-    })
+// setup route for all departments
+app.get("/departments", function (req, res) {
+  service.getDepartments().then(function (data) {
+    res.json(data);
+  }).catch((err) => {
+    res.json({ message: err });
+  })
+});
+// send a status code and a message when going to a route that's not included
+app.use(function (req, res) {
+  res.status(404);
+  res.send("Page Not found");
 });
 
 // setup http server to listen on HTTP_PORT
-service.initialize().then(()=>{
+service.initialize().then(() => {
   app.listen(HTTP_PORT, onHttpStart);
-}).catch( ()=>{
+}).catch(() => {
   console.log("Error initializing.");
 })
